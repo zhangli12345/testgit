8f2ee730aac75d28ea62bfdb100b85dd86116365
‘zhangli12345’ <1208631086@qq.com>
2019-03-05 12:00:12 +0800

    App和User改动的

--- src/main/java/com/neusoft/App.java
+++ src/main/java/com/neusoft/App.java
@@ -10,5 +10,6 @@ public class App
     {
 
         System.out.println( "Hello World!２" );
+        System.out.println("李四改动的");
     }
 }
--- src/main/java/com/neusoft/User.java
+++ src/main/java/com/neusoft/User.java
@@ -2,4 +2,5 @@ package com.neusoft;
 
 public class User {
 
+
 }
