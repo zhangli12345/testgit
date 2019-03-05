6e27d161f187cefd311968ce0adb29610762facf
‘zhangli12345’ <1208631086@qq.com>
2019-03-05 14:30:37 +0800

    zl1改动的

--- src/main/java/com/neusoft/App.java
+++ src/main/java/com/neusoft/App.java
@@ -9,11 +9,7 @@ public class App
     public static void main( String[] args )
     {
 
-
-
-
         System.out.println( "Hello World!２" );
         System.out.println("李四改动的");
-        System.out.println("zahnkgio");
     }
 }
